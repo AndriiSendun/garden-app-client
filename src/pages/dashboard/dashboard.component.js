a4f93ead9931ad949d0266022accd7e7d08ae383
Pavlo Pihula <rogertaylor1949@gmail.com>
2019-05-20 15:40:06 -0400

    Update dashboard filters

--- src/pages/dashboard/dashboard.component.js
+++ src/pages/dashboard/dashboard.component.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import ReactTooltip from "react-tooltip";
+import ReactTooltip from 'react-tooltip';
 
-import axios from "axios";
+import axios from 'axios';
 
-import "./dashboard.styles.scss";
-import { Button, TYPES } from "../../common/components/button/button.component";
+import './dashboard.styles.scss';
+import { Button, TYPES } from '../../common/components/button/button.component';
 
-import Header from "../../common/header/header.component";
-import Footer from "../../common/footer/footer.component";
-import PageTitle from "../../common/page-title/page-title.component";
+import Header from '../../common/header/header.component';
+import Footer from '../../common/footer/footer.component';
+import PageTitle from '../../common/page-title/page-title.component';
 
-import Sensors from "./sensors.component";
+import Sensors from './sensors.component';
 
-import { backendUrl } from "../../constants/backendUrl";
+import { backendUrl } from '../../constants/backendUrl';
 
-import ArrowUp from "../../assets/arrow-up.svg";
-import ArrowDown from "../../assets/arrow-down.svg";
+import ArrowUp from '../../assets/arrow-up.svg';
+import ArrowDown from '../../assets/arrow-down.svg';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filter: "",
+      filter: '',
       flowers: [],
-      sortBy: "name",
+      sortBy: 'name',
       isAscendingSort: true,
       isProblematicSort: true,
-      filterDisconnected: true,
-      isConnected: false
+      filterDisconnected: true
     };
     this.flower = [];
     this.issues = [];
+    this.globalConnected = [];
     this.onFilter = this.onFilter.bind(this);
     this.onSort = this.onSort.bind(this);
     this.onSelect = this.onSelect.bind(this);
@@ -41,7 +41,7 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    const curretnUser = JSON.parse(`${localStorage.getItem("state")}`)
+    const curretnUser = JSON.parse(`${localStorage.getItem('state')}`)
       .authReducer.user.id;
 
     axios
@@ -81,25 +81,16 @@ class Dashboard extends React.Component {
     });
   }
 
-  sortConnected = () => {
-    this.setState(({ flowers, isConnected }) => {
-      const active = flowers.filter(flower => flower.package_id !== "");
-
-      return {
-        flowers: !isConnected && active.length ? [...active] : this.flower,
-        isConnected: !isConnected
-      };
-    });
-  };
-
   sortDisconnected = () => {
     this.setState(({ flowers, filterDisconnected }) => {
-      const disconnected = flowers.filter(flower => flower.package_id === "");
+      const disconnected = flowers.filter(flower => flower.package_id === '');
 
       return {
         flowers:
-          filterDisconnected && disconnected.length
-            ? [...disconnected]
+          filterDisconnected && !this.globalConnected.length
+            ? disconnected.length
+              ? [...disconnected]
+              : ['']
             : this.flower,
         filterDisconnected: !filterDisconnected
       };
@@ -107,34 +98,40 @@ class Dashboard extends React.Component {
   };
 
   sortByProblems = () => {
-    const flower = this.issues
-      .map(flow => flow.id)
-      .reduce((obj, current) => {
-        obj[current] ? obj[current]++ : (obj[current] = 2);
-        return obj;
-      }, {});
-
-    const order = Object.keys(flower).sort((a, b) => flower[b] - flower[a]);
     this.setState(({ flowers, isProblematicSort }) => {
-      const itemsToFilter = flowers.filter(flower => flower.package_id !== "");
-      const rest = flowers.filter(flower => flower.package_id === "");
-      if (isProblematicSort) {
-        itemsToFilter.sort(
-          (a, b) => order.indexOf(a._id) - order.indexOf(b._id)
-        );
-      } else {
-        itemsToFilter.sort(
-          (a, b) => order.indexOf(b._id) - order.indexOf(a._id)
-        );
-      }
-      const filtered = [...itemsToFilter, ...rest];
-      return { flowers: filtered, isProblematicSort: !isProblematicSort };
+      const problematicFlowers = flowers.reduce((problematic, current) => {
+        this.issues
+          .filter(flower => flower.problematic === true)
+          .forEach(issue => {
+            if (issue.id === current._id) {
+              problematic.push(current);
+            }
+          });
+
+        return problematic;
+      }, []);
+
+      return {
+        flowers: isProblematicSort
+          ? problematicFlowers.length
+            ? [...problematicFlowers]
+            : ['']
+          : this.flower,
+        isProblematicSort: !isProblematicSort
+      };
     });
   };
 
   renderThumbnails(data) {
     return data.map(flower => {
-      return <Sensors flower={flower} key={flower._id} issues={this.issues} />;
+      return (
+        <Sensors
+          flower={flower}
+          key={flower._id}
+          issues={this.issues}
+          globalConnected={this.globalConnected}
+        />
+      );
     });
   }
 
@@ -142,85 +139,93 @@ class Dashboard extends React.Component {
     return !this.state.flowers.length ? (
       <p>There are no flowers yet</p>
     ) : (
-      <p>No mathes</p>
+      <p>No matches</p>
     );
   }
 
   render() {
     let renderContent;
     const { filter, flowers, isAscendingSort } = this.state;
-    const data = flowers.filter(item =>
-      item.name.toLowerCase().match(filter.toLowerCase())
-    );
+    const data = flowers.filter(item => {
+      if (item.name) {
+        return item.name.toLowerCase().match(filter.toLowerCase());
+      }
+    });
 
     if (this.state.flowers.length) {
       renderContent = (
         <>
-          <section className="dashboard">
-            <div className="dashboard__search">
-              <label className="dashboard__search__title">Search</label>
+          <section className='dashboard'>
+            <div className='dashboard__search'>
+              <label className='dashboard__search__title'>Search</label>
               <input
-                data-tip="Search for flowers..."
-                className="dashboard__search--field"
-                type="text"
-                placeholder="Flower name..."
+                data-tip='Search for flowers...'
+                className='dashboard__search--field'
+                type='text'
+                placeholder='Flower name...'
                 value={filter}
                 onChange={this.onFilter}
               />
             </div>
 
-            <div className="dashboard--sorting">
-              <span className="dashboard--sorting__title">Sort by</span>
+            <div className='dashboard--sorting'>
+              <span className='dashboard--sorting__title'>Sort by</span>
 
-              <div className="dashboard--sorting--alphabetical">
-                <label data-tip="Sort flowers by name" htmlFor='alphabetical-sorting' className='dashboard--sorting__label'>
+              <div className='dashboard--sorting--alphabetical'>
+                <label
+                  data-tip='Sort flowers by name'
+                  htmlFor='alphabetical-sorting'
+                  className='dashboard--sorting__label'>
                   Alphabetical
-                  <button className="dashboard--sorting__button"  onClick={this.onSort} id='alphabetical-sorting'>
+                  <button
+                    className='dashboard--sorting__button'
+                    onClick={this.onSort}
+                    id='alphabetical-sorting'>
                     <img
                       className={
-                        isAscendingSort ? "icon icon--up" : "icon icon--down"
+                        isAscendingSort ? 'icon icon--up' : 'icon icon--down'
                       }
                       src={isAscendingSort ? ArrowUp : ArrowDown}
-                      alt="sort by alphabet"
+                      alt='sort by alphabet'
                     />
                   </button>
                 </label>
               </div>
             </div>
-            <div className="dashboard--sorting">
-              <span className="dashboard--sorting__title">Filter by</span>
-              <div className="dashboard--sorting--problematical">
-                <div className="dashboard--sorting--problematical--problematical">
+            <div className='dashboard--sorting'>
+              <span className='dashboard--sorting__title'>Filter by</span>
+              <div className='dashboard--sorting--problematical'>
+                <div className='dashboard--sorting--problematical--problematical'>
                   <input
-                    id="problematical"
-                    type="checkbox"
-                    onClick={this.sortConnected}
-                    className="checkbox"
+                    id='problematical'
+                    type='checkbox'
+                    onClick={this.sortByProblems}
+                    className='checkbox'
                   />
-                  <label htmlFor="problematical" className="checkbox__label">
+                  <label htmlFor='problematical' className='checkbox__label'>
                     Problematical
                   </label>
                 </div>
-                <div className="dashboard--sorting--problematical--disconnected">
+                <div className='dashboard--sorting--problematical--disconnected'>
                   <input
-                    id="disconnected"
-                    type="checkbox"
-                    onClick={this.sortConnected}
-                    className="checkbox"
+                    id='disconnected'
+                    type='checkbox'
+                    onClick={this.sortDisconnected}
+                    className='checkbox'
                   />
-                  <label htmlFor="disconnected" className="checkbox__label">
+                  <label htmlFor='disconnected' className='checkbox__label'>
                     Disconnected
                   </label>
                 </div>
               </div>
             </div>
           </section>
-          <div className="dashboard--flower-list">
-            <h2 className="dashboard--flower-list__title">Flower list</h2>
-            <Link to="/create" className="dashboard__link">
+          <div className='dashboard--flower-list'>
+            <h2 className='dashboard--flower-list__title'>Flower list</h2>
+            <Link to='/create' className='dashboard__link'>
               CREATE FLOWER
             </Link>
-            <div className="dashboard--thumbnail">
+            <div className='dashboard--thumbnail'>
               {data.length
                 ? this.renderThumbnails(data)
                 : this.renderFallbackMessage()}
@@ -230,11 +235,11 @@ class Dashboard extends React.Component {
       );
     } else {
       renderContent = (
-        <div className="empty-dashboard">
-          <p className="empty-dashboard__title">You have no flowers</p>
-          <div className="empty-dashboard--create">
-            <Link to="/create" className="create__link">
-              <Button buttonType={TYPES.PRIMARY} title="CREATE FLOWER" />
+        <div className='empty-dashboard'>
+          <p className='empty-dashboard__title'>You have no flowers</p>
+          <div className='empty-dashboard--create'>
+            <Link to='/create' className='create__link'>
+              <Button buttonType={TYPES.PRIMARY} title='CREATE FLOWER' />
             </Link>
           </div>
         </div>
@@ -242,8 +247,8 @@ class Dashboard extends React.Component {
     }
 
     return (
-      <div className="dashboard--page">
-        <PageTitle title="Dashboard" />
+      <div className='dashboard--page'>
+        <PageTitle title='Dashboard' />
         <Header />
         {renderContent}
         <Footer />
